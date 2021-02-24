da4cf90b642ce92bd20878956a594a9649ec76d8
irenemanas <irenemanas@gmail.com>
2021-02-24 09:18:01 +0100

    add PokeList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import "../style/App.scss";
 import React from "react";
+import pokemons from "../data/data.json";
+import PokeList from "./PokeList";
 
 class App extends React.Component {
   render() {
-    return <div>Hola Mundo</div>;
+    return <PokeList pokemons={pokemons} />;
   }
 }
 
---
+++ src/components/PokeList.js
@@ -0,0 +1,10 @@
+import "../style/App.scss";
+import React from "react";
+
+class PokeList extends React.Component {
+  render() {
+    return console.log(PokeList);
+  }
+}
+
+export default PokeList;
