bd8a1045ca6edc7334558fc19adae0901f7d8fbb
irenemanas <irenemanas@gmail.com>
2021-02-24 11:05:31 +0100

    run build

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -5,7 +5,9 @@ function Pokemon(props) {
     <article className='page-list__item'>
       <img src={props.pokProp.url} alt='Pokemon' className='page-list__item--image' />
       <h3 className='page-list__item--title'>{props.pokProp.name}</h3>
-      <p className='page-list__item--type'>{props.pokProp.types}</p>
+      <p className='page-list__item--type'>
+        {props.pokProp.types[0]} {props.pokProp.types[1]}
+      </p>
     </article>
   );
 }
